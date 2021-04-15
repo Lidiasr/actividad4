523fd0e7f825f10ed57e64f307e4b5672b06ed2d
Usulidia <lidia@example.com>
2021-04-15 09:15:46 +0200

    Creo primera frase en cambio1

--- actividad4/src/actividad4/cambio1.java
+++ actividad4/src/actividad4/cambio1.java
@@ -1,5 +1,9 @@
 package actividad4;
 
-public class cambio1 {
+import java.io.IOException;
 
+public class cambio1 {
+	public static void main(String[] args) throws IOException {
+		System.out.println(" ------------NUEVA CLASE------------");
+	}
 }
