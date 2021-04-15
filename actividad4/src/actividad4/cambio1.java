0d32473b6c0c0bb1c31966434d25623d5657d692
Usulidia <lidia@example.com>
2021-04-15 09:16:24 +0200

    Empieza el cambio1

--- actividad4/src/actividad4/cambio1.java
+++ actividad4/src/actividad4/cambio1.java
@@ -5,5 +5,6 @@ import java.io.IOException;
 public class cambio1 {
 	public static void main(String[] args) throws IOException {
 		System.out.println(" ------------NUEVA CLASE------------");
+		System.out.println(" ------------Empieza el cambio1------------");
 	}
 }
